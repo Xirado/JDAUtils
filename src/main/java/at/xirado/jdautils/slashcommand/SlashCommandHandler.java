d191bd6ec4d51a28dff4402ee52bd764ab874cfd
xirado <bquickzz0@gmail.com>
2021-06-06 00:40:33 +0200

    Added option to set ExecutorService on SlashCommandHandlerBuilder

--- src/main/java/at/xirado/jdautils/slashcommand/SlashCommandHandler.java
+++ src/main/java/at/xirado/jdautils/slashcommand/SlashCommandHandler.java
@@ -1,5 +1,6 @@
 package at.xirado.jdautils.slashcommand;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.GenericEvent;
@@ -11,17 +12,21 @@ import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class SlashCommandHandler implements EventListener
 {
+    private final ExecutorService executorService;
     private final Set<SlashCommand> guildSlashCommands;
     private final Set<SlashCommand> globalSlashCommands;
     private boolean jdaReady = false;
     private JDA jda;
 
-    public SlashCommandHandler(Set<SlashCommand> slashCommandSet)
+    protected SlashCommandHandler(Set<SlashCommand> slashCommandSet, ExecutorService executorService)
     {
+        this.executorService = executorService != null ? executorService : Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors(), new ThreadFactoryBuilder().setNameFormat("SlashCommand Thread %d").build());
         globalSlashCommands = slashCommandSet.stream().filter(SlashCommand::isGlobal).collect(Collectors.toSet());
         guildSlashCommands = slashCommandSet.stream().filter(x -> !x.isGlobal() && x.getEnabledGuilds().size() >= 1).collect(Collectors.toSet());
     }
@@ -29,59 +34,61 @@ public class SlashCommandHandler implements EventListener
     @Override
     public void onEvent(@NotNull GenericEvent event)
     {
-
-        if(!jdaReady && event instanceof ReadyEvent)
-        {
-            ReadyEvent readyEvent = (ReadyEvent) event;
-            jdaReady = true;
-            jda = readyEvent.getJDA();
-            CommandListUpdateAction commandListUpdateAction = jda.updateCommands();
-            for(SlashCommand slashCommand : globalSlashCommands)
+        Runnable r = () -> {
+            if(!jdaReady && event instanceof ReadyEvent)
             {
-                commandListUpdateAction.addCommands(slashCommand.getCommandData());
-            }
-            commandListUpdateAction.queue();
-            return;
-        }
-
-        if(event instanceof GuildReadyEvent)
-        {
-            GuildReadyEvent guildReadyEvent = (GuildReadyEvent) event;
-            Guild guild = guildReadyEvent.getGuild();
-            Set<SlashCommand> slashCommands = guildSlashCommands.stream()
-                    .filter(x -> x.getEnabledGuilds().contains(guild.getIdLong()))
-                    .collect(Collectors.toSet());
-            if(slashCommands.size() >= 1)
-            {
-                CommandListUpdateAction commandListUpdateAction = guild.updateCommands();
-                for(SlashCommand slashCommand : slashCommands)
+                ReadyEvent readyEvent = (ReadyEvent) event;
+                jdaReady = true;
+                jda = readyEvent.getJDA();
+                CommandListUpdateAction commandListUpdateAction = jda.updateCommands();
+                for(SlashCommand slashCommand : globalSlashCommands)
                 {
                     commandListUpdateAction.addCommands(slashCommand.getCommandData());
                 }
                 commandListUpdateAction.queue();
                 return;
             }
-        }
-        
-        if(event instanceof SlashCommandEvent)
-        {
-            SlashCommandEvent slashCommandEvent = (SlashCommandEvent) event;
-            String name = slashCommandEvent.getName();
-            SlashCommand slashCommand = globalSlashCommands.stream()
-                    .filter(x -> x.getCommandData().getName().equalsIgnoreCase(name))
-                    .findFirst()
-                    .orElse(null);
-            if(slashCommand != null)
+
+            if(event instanceof GuildReadyEvent)
             {
-                slashCommand.executeSlashCommand(slashCommandEvent);
-                return;
+                GuildReadyEvent guildReadyEvent = (GuildReadyEvent) event;
+                Guild guild = guildReadyEvent.getGuild();
+                Set<SlashCommand> slashCommands = guildSlashCommands.stream()
+                        .filter(x -> x.getEnabledGuilds().contains(guild.getIdLong()))
+                        .collect(Collectors.toSet());
+                if(slashCommands.size() >= 1)
+                {
+                    CommandListUpdateAction commandListUpdateAction = guild.updateCommands();
+                    for(SlashCommand slashCommand : slashCommands)
+                    {
+                        commandListUpdateAction.addCommands(slashCommand.getCommandData());
+                    }
+                    commandListUpdateAction.queue();
+                    return;
+                }
             }
-            if(slashCommandEvent.isFromGuild())
+
+            if(event instanceof SlashCommandEvent)
             {
-                guildSlashCommands.stream()
-                        .filter(x -> x.getCommandData().getName().equalsIgnoreCase(name) && x.canBeExecuted(slashCommandEvent.getGuild().getIdLong()))
-                        .findFirst().ifPresent(guildslashCommand -> guildslashCommand.executeSlashCommand(slashCommandEvent));
+                SlashCommandEvent slashCommandEvent = (SlashCommandEvent) event;
+                String name = slashCommandEvent.getName();
+                SlashCommand slashCommand = globalSlashCommands.stream()
+                        .filter(x -> x.getCommandData().getName().equalsIgnoreCase(name))
+                        .findFirst()
+                        .orElse(null);
+                if(slashCommand != null)
+                {
+                    slashCommand.executeSlashCommand(slashCommandEvent);
+                    return;
+                }
+                if(slashCommandEvent.isFromGuild())
+                {
+                    guildSlashCommands.stream()
+                            .filter(x -> x.getCommandData().getName().equalsIgnoreCase(name) && x.canBeExecuted(slashCommandEvent.getGuild().getIdLong()))
+                            .findFirst().ifPresent(guildslashCommand -> guildslashCommand.executeSlashCommand(slashCommandEvent));
+                }
             }
-        }
+        };
+        executorService.submit(r);
     }
 }
--- src/main/java/at/xirado/jdautils/slashcommand/SlashCommandHandlerBuilder.java
+++ src/main/java/at/xirado/jdautils/slashcommand/SlashCommandHandlerBuilder.java
@@ -5,14 +5,22 @@ import net.dv8tion.jda.internal.utils.Checks;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
 
 public class SlashCommandHandlerBuilder
 {
     private Set<SlashCommand> addedSlashCommands = new HashSet<>();
+    private ExecutorService executorService;
 
     public SlashCommandHandlerBuilder()
     {
 
+    }
+
+    public SlashCommandHandlerBuilder setExecutorService(ExecutorService executorService)
+    {
+        this.executorService = executorService;
+        return this;
     }
 
     public SlashCommandHandlerBuilder registerSlashCommands(SlashCommand... slashCommands)
@@ -24,6 +32,6 @@ public class SlashCommandHandlerBuilder
 
     public SlashCommandHandler build()
     {
-        return new SlashCommandHandler(addedSlashCommands);
+        return new SlashCommandHandler(addedSlashCommands, executorService);
     }
 }
